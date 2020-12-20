de63f3ecbff72a0486f527de241891edcab52032
Henrique Fernandes <henriquefernandes@Air-de-Henrique.Home>
2020-12-20 23:15:16 +0100

    add strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hefernan <hefernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 00:10:06 by hefernan          #+#    #+#             */
-/*   Updated: 2020/12/18 01:13:14 by hefernan         ###   ########.fr       */
+/*   Updated: 2020/12/20 23:14:52 by hefernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,30 @@
 
 static int	intermilan(char c, char *str)
 {
-    int		i;
+	int		i;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == c)
-            return (i);
-        i++;
-    }
-    return (-1);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
 char		*ft_strtrim(char const *s1, char const *set)
 {
-    int		dubut;
-    int		fin;
-    char	*str;
+	int		dubut;
+	int		fin;
 
-    dubut = 0;
-    fin = ft_strlen(s1);
-
+	dubut = 0;
+	fin = ft_strlen(s1);
+	while (intermilan(s1[dubut], (char *)set) >= 0)
+		dubut++;
+	if ((fin - dubut) <= 0)
+		return (ft_substr(s1, 0, 0));
+	while (intermilan(s1[fin - 1], (char *)set) >= 0)
+		fin--;
+	return (ft_substr(s1, dubut, fin - dubut));
 }
